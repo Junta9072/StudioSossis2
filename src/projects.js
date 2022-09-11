7556976d04ec00aa2663d0d071c373e542a14b1f
Junta9072 <legoblok2709@gmail.com>
2022-09-12 00:41:03 +0200

    removed uneccesary js, added google font

--- src/projects.js
+++ src/projects.js
@@ -1,13 +1,1 @@
-let currentString;
-let previousPass;
-let projects = document.querySelectorAll(".project__container");
 
-projects.forEach((item, i) => {
-  console.log(item.style.gridArea);
-  currentString = item.style.gridArea;
-  previousPass = currentString.indexOf("span 1");
-  console.log(currentString.indexOf("span 1", previousPass + 6));
-  if (currentString.indexOf("span 1", previousPass + 6) != -1) {
-    item.style.color = "transparent";
-  }
-});
